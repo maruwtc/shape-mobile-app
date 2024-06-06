3dc5a95924e9128bfc5734a2057aba103b236060
maruwtc <chriswong0621@gmail.com>
2024-06-06 19:12:27 +0800

    chore: Add audio recording functionality using expo-av plugin

--- app/(tabs)/speech.tsx
+++ app/(tabs)/speech.tsx
@@ -1,29 +1,92 @@
-import React from 'react';
-import { Keyboard, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Audio } from 'expo-av';
 import Header from '@/components/Header';
 import {
-    Text,
+    Button
 } from "react-native-paper";
 
-const Speech = () => {
+export default function App() {
+    const [sound, setSound] = useState();
+    const [recording, setRecording] = useState();
+    const [recordingUri, setRecordingUri] = useState(null);
+    const [permissionResponse, requestPermission] = Audio.usePermissions();
+
+    async function playSound() {
+        if (recordingUri) {
+            console.log('Loading Sound');
+            const { sound } = await Audio.Sound.createAsync({ uri: recordingUri });
+            setSound(sound);
+
+            console.log('Playing Sound');
+            await sound.playAsync();
+        } else {
+            console.log('No recording available to play');
+        }
+    }
+
+    useEffect(() => {
+        return sound
+            ? () => {
+                console.log('Unloading Sound');
+                sound.unloadAsync();
+            }
+            : undefined;
+    }, [sound]);
+
+    async function startRecording() {
+        try {
+            if (permissionResponse.status !== 'granted') {
+                console.log('Requesting permission..');
+                await requestPermission();
+            }
+            await Audio.setAudioModeAsync({
+                allowsRecordingIOS: true,
+                playsInSilentModeIOS: true,
+            });
+
+            console.log('Starting recording..');
+            const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
+            setRecording(recording);
+            console.log('Recording started');
+        } catch (err) {
+            console.error('Failed to start recording', err);
+        }
+    }
+
+    async function stopRecording() {
+        console.log('Stopping recording..');
+        await recording.stopAndUnloadAsync();
+        await Audio.setAudioModeAsync({
+            allowsRecordingIOS: false,
+        });
+        const uri = recording.getURI();
+        setRecordingUri(uri);
+        setRecording(undefined);
+        console.log('Recording stopped and stored at', uri);
+    }
+
     return (
         <>
-            <Header title="Home" />
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.container}>
-                    <Text>Speech</Text>
-                </View>
-            </TouchableWithoutFeedback>
+            <Header title="OCR" />
+            <View style={styles.container}>
+                <Button
+                    style={styles.button}
+                    onPress={recording ? stopRecording : startRecording}
+                >{recording ? 'Stop Recording' : 'Start Recording'}</Button>
+                <Button style={styles.button} onPress={playSound}>Play Last Recording</Button>
+            </View>
         </>
     );
 }
-
-export default Speech;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    button: {
+        marginBottom: 10,
     },
 });
