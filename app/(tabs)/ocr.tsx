630b01110f822ebee9971b50208abf866dd8bd33
maruwtc <chriswong0621@gmail.com>
2024-06-06 19:12:23 +0800

    chore: Add camera functionality using expo-camera plugin

--- app/(tabs)/ocr.tsx
+++ app/(tabs)/ocr.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
-import { Keyboard, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import Header from '@/components/Header';
+import { Camera, CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
     Text,
+    Button
 } from "react-native-paper";
 
 const OCR = () => {
+    const [permission, requestPermission] = useCameraPermissions();
+    const [photoUri, setPhotoUri] = React.useState(null);
+    const cameraRef = React.useRef(null);
+
+    if (!permission) {
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        return (
+            <>
+                <Header title="OCR" />
+                <View style={styles.container}>
+                    <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+                    <Button onPress={requestPermission} mode="contained" style={{ margin: 20 }}>Grant permission</Button>
+                </View>
+            </>
+        );
+    }
+
+    const takePhoto = async () => {
+        setPhotoUri(null);
+        const camera = cameraRef.current;
+        if (!camera) return;
+        try {
+            const data = await camera.takePictureAsync();
+            setPhotoUri(data.uri);
+            console.log('photo: ', photoUri);
+        } catch (error) {
+            console.log('error: ', error);
+        }
+    }
+
     return (
         <>
-            <Header title="Home" />
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.container}>
-                    <Text>OCR</Text>
-                </View>
-            </TouchableWithoutFeedback>
+            <Header title="OCR" />
+            <View style={styles.container}>
+                <CameraView style={styles.camera} facing='back'>
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={styles.button} onPress={takePhoto}>
+                            <Button
+                                mode="contained"
+                                onPress={takePhoto}
+                                style={styles.cameraButton}
+                            ><Icon name="camera" style={styles.icon} /></Button>
+                        </TouchableOpacity>
+                    </View>
+                </CameraView>
+            </View>
         </>
     );
 }
@@ -25,5 +70,34 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    camera: {
+        flex: 1,
+        width: '100%',
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 4,
+
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    cameraButton: {
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        height: 65,
+        width: 65,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 100,
+        marginBottom: 20,
+    },
+    icon: {
+        color: 'white',
+        fontSize: 20,
     },
 });
