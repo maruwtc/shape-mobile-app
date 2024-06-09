e1524cd52786701068be3b3ac268dfcd03047618
maruwtc <chriswong0621@gmail.com>
2024-06-10 02:53:39 +0800

    refactor: Optimize DropdownList component for better performance

--- app/(tabs)/ocr.tsx
+++ app/(tabs)/ocr.tsx
@@ -1,103 +1,114 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { TouchableOpacity, View, Image } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
 import Header from '@/components/Header';
-import { Camera, CameraView, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {
-    Text,
-    Button
-} from "react-native-paper";
+import { Appbar, IconButton, Button, Text, Card, TextInput, Divider, Modal } from 'react-native-paper';
+import { AutoFocus } from 'expo-camera/build/legacy/Camera.types';
 
-const OCR = () => {
+export default function App() {
     const [permission, requestPermission] = useCameraPermissions();
-    const [photoUri, setPhotoUri] = React.useState(null);
-    const cameraRef = React.useRef(null);
+    const [showCamera, setShowCamera] = useState(false);
+    const [photoUri, setPhotoUri] = useState<string | null>(null);
+    const camera = useRef<CameraView | null>(null);
+    const [lastPictureModal, setLastPictureModal] = useState(false);
+    const showlastPictureModal = () => setLastPictureModal(true);
+    const hidelastPictureModal = () => setLastPictureModal(false);
 
     if (!permission) {
         return <View />;
     }
 
-    if (!permission.granted) {
+    const takePicture = async () => {
+        try {
+            const options = { quality: 0.5, base64: true };
+            const data = await (AutoFocus.on, camera.current?.takePictureAsync(options) ?? Promise.resolve());
+            console.log(data ? data.uri : "No data");
+            setPhotoUri(data ? data.uri : null);
+            setShowCamera(false);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const uploadPicture = () => {
+        console.log("Uploading picture");
+    };
+
+    const cameraView = () => {
+        if (!permission.granted) {
+            return (
+                <>
+                    <Card className='flex-1 justify-center items-center w-full h-full'>
+                        <Text className='text-center'>We need your permission to show the camera</Text>
+                        <Button onPress={requestPermission}>Allow Camera</Button>
+                    </Card>
+                </>
+            );
+        } else {
+            return (
+                <CameraView ref={camera} className="flex-1 w-full min-w-full">
+                    <View className='flex-1 flex-row bg-transparent m-4'>
+                        <TouchableOpacity onPress={takePicture} className='flex-1 self-end items-center'>
+                            <IconButton mode='outlined' icon="moon-full" size={48} />
+                        </TouchableOpacity>
+                    </View>
+                </CameraView>
+            );
+        }
+    };
+
+    const openCameraView = () => {
+        setShowCamera(true);
+    };
+
+    if (showCamera) {
         return (
             <>
-                <Header title="OCR" />
-                <View style={styles.container}>
-                    <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-                    <Button onPress={requestPermission} mode="contained" style={{ margin: 20 }}>Grant permission</Button>
-                </View>
+                <Appbar.Header>
+                    <Appbar.BackAction onPress={() => { setShowCamera(false) }} />
+                    <Appbar.Content title="Camera" />
+                </Appbar.Header>
+                {cameraView()}
             </>
         );
-    }
-
-    const takePhoto = async () => {
-        setPhotoUri(null);
-        const camera = cameraRef.current;
-        if (!camera) return;
-        try {
-            const data = await camera.takePictureAsync();
-            setPhotoUri(data.uri);
-            console.log('photo: ', photoUri);
-        } catch (error) {
-            console.log('error: ', error);
-        }
     }
 
     return (
         <>
             <Header title="OCR" />
-            <View style={styles.container}>
-                <CameraView style={styles.camera} facing='back'>
-                    <View style={styles.buttonContainer}>
-                        <TouchableOpacity style={styles.button} onPress={takePhoto}>
-                            <Button
-                                mode="contained"
-                                onPress={takePhoto}
-                                style={styles.cameraButton}
-                            ><Icon name="camera" style={styles.icon} /></Button>
-                        </TouchableOpacity>
-                    </View>
-                </CameraView>
-            </View>
+            <Card className='flex-1 justify-center items-center w-full h-full'>
+                <Card.Content className='justify-center items-center'>
+                    <TextInput
+                        mode="outlined"
+                        label="Text in Image"
+                        value=""
+                        className="w-full min-h-[180px] max-h-[180px]"
+                        editable={false}
+                        multiline
+                    />
+                    <Divider className='w-full my-4 bg-gray-500' />
+                    <TextInput
+                        mode="outlined"
+                        label="Translated Text"
+                        value=""
+                        className="w-full min-h-[180px] max-h-[180px]"
+                        editable={false}
+                        multiline
+                    />
+                </Card.Content>
+                <Card.Actions className='justify-between items-center my-4'>
+                    <Button onPress={uploadPicture}>Upload Picture</Button>
+                    <Button onPress={openCameraView}>Open Camera</Button>
+                </Card.Actions>
+                <Text className='text-center'>{photoUri ? 'Image saved' : "No Text"}</Text>
+                {photoUri && <Button onPress={showlastPictureModal}>Check Image</Button>}
+                <Modal visible={lastPictureModal} onDismiss={hidelastPictureModal}
+                    contentContainerStyle={{ backgroundColor: 'white', paddingHorizontal: 20, paddingVertical: 40, margin: 20, borderRadius: 10 }}
+                >
+                    <Image source={photoUri ? { uri: photoUri } : { uri: 'https://picsum.photos/700' }} className='w-full h-full' />
+                    <Button onPress={hidelastPictureModal} className='mt-4'>Close</Button>
+                </Modal>
+            </Card>
         </>
     );
-}
-
-export default OCR;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    camera: {
-        flex: 1,
-        width: '100%',
-    },
-    buttonContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: 'transparent',
-        margin: 4,
-
-    },
-    button: {
-        flex: 1,
-        alignSelf: 'flex-end',
-        alignItems: 'center',
-    },
-    cameraButton: {
-        backgroundColor: 'rgba(0,0,0,0.3)',
-        height: 65,
-        width: 65,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 100,
-        marginBottom: 20,
-    },
-    icon: {
-        color: 'white',
-        fontSize: 20,
-    },
-});
+};
